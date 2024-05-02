c72e279d2ba0b0ea910887294203a08430bfc7e7
Alan Corrêa <souzaalanc75@gmail.com>
2024-05-02 16:05:25 -0300

    adicionado script que builda o executável

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from cx_Freeze import setup, Executable
 
 
-setup(
-    name='Calculadora',
-    version='0.1.1',
-    authors=["AlanSouza-19 <souzaalanc75@gmail.com>"],
-    description='Calculadora feita em python com a lib TKinter',
-    options={
-        'build_exe': {
-            'includes': ['tkinter', 'math', 're', 'typing'],
-        }
-    },
-    executables=[
-        Executable(
-            script='./calculadora/main.py',
-            base='Win32Gui',
-            icon='./icons/calculator-icon',
-            target_name='Calculadora'    
-        )
-    ]
-)
+def main():
+    setup(
+        name='Calculadora',
+        version='0.1.1',
+        authors=["AlanSouza-19 <souzaalanc75@gmail.com>"],
+        description='Calculadora feita em python com a lib TKinter',
+        options={
+            'build_exe': {
+                'includes': ['tkinter', 'math', 're', 'typing'],
+            }
+        },
+        executables=[
+            Executable(
+                script='./calculadora/main.py',
+                base='Win32Gui',
+                icon='./icons/calculator-icon',
+                target_name='Calculadora'    
+            )
+        ]
+    )
+
+if __name__ == '__main__':
+    main()
